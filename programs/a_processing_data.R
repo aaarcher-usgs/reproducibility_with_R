7d7f9d61836e126eaced789cf32ba96f12ccefda
aaarchmiller <althea.archmiller@gmail.com>
2016-10-24 13:03:29 -0500

    formatting processing data example

--- programs/a_processing_data.R
+++ programs/a_processing_data.R
@@ -4,13 +4,15 @@
 #' 
 #' **Read CSV**
 rawdata <- read.csv("data/raw.csv")
-head(rawdata)
+head(rawdata,n = 3)
 
 #' **Clean up**
 #' 
 #' 1. Remove NAs
 #' 2. Remove outliers
+# Remove missing values in X column
 data.noNA <- rawdata[! is.na(rawdata$X),]
-# Plot data to check for outliers
+#' Plot data to check for outliers
 plot(x = data.noNA$X, y = data.noNA$Y)
+#' Remove outliers
 cleandata <- data.noNA[data.noNA$Y!=14,]
